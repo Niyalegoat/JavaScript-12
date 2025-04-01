07eabc7aa7abc8dc978d567a8f58cc944bc4c98d
Niyalegoat <167811185+Niyalegoat@users.noreply.github.com>
2025-04-01 10:02:28 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+const isLoggedIn = true;
+const isAdmin = false;
+
+if(isLOggedIn && isAdmin) {
+    console.log("Welcome, Admin!");
+}else {
+    console.log("Access denied.");
+}
+
+const hasSearchTerm = false;
+const hasCategorySelected = true;
+if(hasSearchTerm || hasCategorySelected){
+    console.log("Filters applied.");
+}
+
+const isBanned = false;
+const isActive = false;
+if(isBanned || isActive){
+    console.log("Hide submit button");
+}
+const loggedIn = true;
+const isSubscribed = true;
+
+if(loggedIn &&isSubscribed){
+    console.log("Show premium content.");
+}
+
+const discount= 10;
+const isHoliday= true;
+if(discount > 0 && isHoliday){
+    console.log("Apply holiday discounty.");
+}
+
+const agreedToTerms = fale;
+const isHoliday=  true;
+if(!agreeToTerm){
+    console.log("You must agree to term first.");
+}
+
+const registeredLessThan7Days = true;
+const profileIncomplete = true;
+
+if(registeredLessThan7Days || profileIncomplete){
+    console.log("WElcome! Finish setting up your profile.");
+}
+
+function checkUserForm() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    if (name !== "" && email !== "") {
+        document.getElementById("forMessage").textContent = "Form complete!";
+    }else{
+        document.getElementById("formMessage").textContent = "Please fill inboth fields.";
+    }
+}
+
+function checkDiscounty() {
+    const checked =
+    document.getElementById("studentDiscount").checked;
+    const quantity = 
+    document.getElementById("quantity").value;
+
+    if (checked && quantity > 2) {
+        document.getElementById("discountMsg").textContent = "Discount Applied!";
+    }else{
+        document.getElementById("discountMsg").textContent = "No discount available.";
+    }
+}
+
+function showBadge() {
+    const status =
+    document.getElementById("status").value;
+    const points = 
+    document.getElementById("points").value;
+
+    if (status === "active" && points >= 100) {
+        document.getElementById("badgeMsg").textContent = "You earned a badge!";
+    }else{
+        document.getElementById("badgeMsg").textContent = "No badge yet.";
+    }
+}
+
+function toggleNotice() {
+    const notfiy =
+    document.getElementById("notifyBox").checked;
+
+    if (!notify) {
+        document.getElementById("notifyBox").textContent = "Notifications are OFF";
+    }else{
+        document.getElementById("badgeMsg").textContent = "Notifications are ON";
+    }
+}
